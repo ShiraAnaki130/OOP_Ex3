aabee8d6238b0d0d8bc64f403c31447102aed83f
lea zuckerman <leahz27@gmail.com>
2021-01-04 22:43:08 +0200

    fix function

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -1,5 +1,6 @@
 from math import inf
-
+import matplotlib.pyplot as plt
+import numpy as np
 from src import GraphAlgoInterface
 from typing import List
 from src import GraphInterface
@@ -14,6 +15,7 @@ class GraphAlgo(GraphAlgoInterface):
 
     def __init__(self, graph: DiGraph = DiGraph()):
         self._graph = graph
+        self._parent = {}
 
     def get_graph(self) -> GraphInterface:
         """
@@ -133,8 +135,65 @@ class GraphAlgo(GraphAlgoInterface):
             path.reverse()
         return nodes[str(id2)].get_tag(), path
 
+    def plot_graph(self) -> None:
+        """            This function plots the graph.
+            If the nodes have a position, the nodes will be placed there.
+            Otherwise, they will be placed in a random but elegant manner.
+            @return: None
+         """
+        all_vertexes = self._graph.get_all_v()
+        x_values = []
+        y_values = []
+        for i in all_vertexes.values():
+            if i.get_pos():
+                x_values.append(i.get_pos()[0])
+                y_values.append(i.get_pos()[1])
+            else:
+                x_random = random.uniform(35.18, 35.2)
+                y_random = random.uniform(32.1, 32.2)
+                i.set_pos((x_random, y_random, 0.0))
+                x_values.append(x_random)
+                y_values.append(y_random)
+        n = [j for j in all_vertexes.keys()]
+        fig, ax = plt.subplots()
+        ax.scatter(x_values, y_values)
+        for p, txt in enumerate(n):
+            ax.annotate(n[p], (x_values[p], y_values[p]))
+        plt.plot(x_values, y_values, "C4o")
+        for i in all_vertexes.keys():
+            for j in self._graph.all_out_edges_of_node(i):
+                x1_coordinate = all_vertexes.get(i).get_pos()[0]
+                y1_coordinate = all_vertexes.get(i).get_pos()[1]
+                x2_coordinate = all_vertexes.get(j).get_pos()[0]
+                y2_coordinate = all_vertexes.get(j).get_pos()[1]
+                plt.arrow(x1_coordinate, y1_coordinate, (x2_coordinate - x1_coordinate), (y2_coordinate - y1_coordinate), length_includes_head=True, width=0.000003, head_width=0.00016, color = 'k')
+        plt.ylabel("y axis")
+        plt.title("OOP_Ex3")
+        plt.xlabel("x axis")
+        plt.show()
+
+    def connected_component(self, id1: int) -> list:
+        nodes = self._graph.get_all_v()
+        self._parent = {}
+        for n in nodes:
+            n.set_tag(-1)
+        node = nodes[str(id1)]
+        self.dfs(node)
+        path = []
+        path.append(node)
+        while node != id1:
+            node = self.parent[str(node.get_key)]
+            path.append(node)
+        path.reverse()
+
+    def dfs(self, v: NodeData):
+        graph = self._graph.get_all_v()
+        edges = self._graph.all_out_edges_of_node(v.get_key())
+        for e in edges.keys():
+            if e not in self._parent:
+                node_e = graph[str(e)]
+                self.parent[str(e)] = v
+                self.dfs(node_e)
 
 
-
-        
 
