c44e2aa757dc52b20410c00480878382fc89a71e
lea zuckerman <leahz27@gmail.com>
2021-01-10 22:15:19 +0200

    change runtime

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import networkx as nx
 def runtime_nx(v, e):
     our_graph_algo = GraphAlgo()
     start_time = time.time()
-    our_graph_algo.load_from_json("../data/G_{}_{}_0.json".format(v,e))
+    our_graph_algo.load_from_json("../data/G_{}_{}_1.json".format(v,e))
     end_time = time.time()
     our_time_load = end_time - start_time
     graph = our_graph_algo.get_graph()
@@ -21,7 +21,7 @@ def runtime_nx(v, e):
             nx_graph.add_edge(i, j, weight=w)
     end_time = time.time()
     nx_time_load = end_time - start_time
-    print("end load graph G_{}_{}_0   our time : {}    networkx time : {}".format(v,e,our_time_load, nx_time_load))
+    print("end load graph G_{}_{}_1   our time : {}    networkx time : {}".format(v,e,our_time_load, nx_time_load))
     start_time = time.time()
     our_graph_algo.connected_components()
     end_time = time.time()
@@ -32,7 +32,7 @@ def runtime_nx(v, e):
         pass
     end_time = time.time()
     nx_time_components = end_time - start_time
-    print("""end connected components algorithm on graph G_{}_{}_0  our time : {}   networkx time : {}""".format(
+    print("""end connected components algorithm on graph G_{}_{}_1  our time : {}   networkx time : {}""".format(
         v, e, our_time_components, nx_time_components))
     start_time = time.time()
     our_graph_algo.shortest_path(0, graph.v_size() - 1)
@@ -43,7 +43,7 @@ def runtime_nx(v, e):
     nx.dijkstra_path_length(nx_graph, 0, graph.v_size() - 1)
     end_time = time.time()
     nx_time_shortest_path = end_time - start_time
-    print("""end shortest_path algorithm on graph G_{}_{}_0 our time : {}  networkx time : {}""".format(
+    print("""end shortest_path algorithm on graph G_{}_{}_1 our time : {}  networkx time : {}""".format(
         v, e, our_time_shortest_path, nx_time_shortest_path))
 
 
@@ -53,25 +53,25 @@ def runtime(v,e):
     graph_algo.load_from_json("../data/G_{}_{}_1.json".format(v, e))
     end_time = time.time()
     time_load = end_time - start_time
-    print("end load graph G_{}_{}_0    time : {} ".format(v, e, time_load,))
+    print("end load graph G_{}_{}_1    time : {} ".format(v, e, time_load,))
     graph = graph_algo.get_graph()
     start_time = time.time()
     graph_algo.connected_component(0)
     end_time = time.time()
     time_component = end_time - start_time
-    print("""end connected component algorithm for node 0  on graph G_{}_{}_0 time : {}  """.format(
+    print("""end connected component algorithm for node 0  on graph G_{}_{}_1 time : {}  """.format(
         v, e, time_component))
     start_time = time.time()
     graph_algo.connected_components()
     end_time = time.time()
     time_components = end_time - start_time
-    print("""end connected components algorithm on graph G_{}_{}_0 time : {}  """.format(
+    print("""end connected components algorithm on graph G_{}_{}_1 time : {}  """.format(
         v, e, time_components,))
     start_time = time.time()
     shortest_path = graph_algo.shortest_path(0, graph.v_size() - 1)
     end_time = time.time()
     time_shortest_path = end_time - start_time
-    print("""end shortest_path algorithm on graph G_{}_{}_0  time : {}  """.format(
+    print("""end shortest_path algorithm on graph G_{}_{}_1  time : {}  """.format(
         v, e, time_shortest_path))
 
 def main():
